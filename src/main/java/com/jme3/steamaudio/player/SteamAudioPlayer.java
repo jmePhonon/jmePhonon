9d4c00a7f231856a154377b364174182f9441e7a
aegroto <aegroto@protonmail.com>
2018-08-25 19:36:27 +0200

    updated test (still not working)

--- src/main/java/com/jme3/steamaudio/player/SteamAudioPlayer.java
+++ src/main/java/com/jme3/steamaudio/player/SteamAudioPlayer.java
@@ -8,19 +8,21 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 public class SteamAudioPlayer {
-    public final static int FRAME_SIZE = 8, CACHE_SIZE = 128;
+    // public final static int FRAME_SIZE = 8, CACHE_SIZE = 128;
 
     public final ByteBuffer audioBuffer;
     public final long size;
+    public final int frameSize;
 
     private SourceDataLine dataLine;
 
-    public SteamAudioPlayer(ByteBuffer audioBuffer, long size,
-                            int sampleRate, int sampleSize) {
+    public SteamAudioPlayer(ByteBuffer audioBuffer, long size, 
+            int sampleRate, int sampleSize, int frameSize, int channels, int frameRate) {
         this.audioBuffer = audioBuffer;
         this.size = size;
+        this.frameSize = frameSize;
 
-        final AudioFormat audioFormat = new AudioFormat(sampleRate, sampleSize, 2, false, false);
+        final AudioFormat audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, sampleRate, sampleSize, channels, frameSize, frameRate, false);
 
         try {
             dataLine = AudioSystem.getSourceDataLine(audioFormat);
@@ -34,11 +36,11 @@ public class SteamAudioPlayer {
 
     public void play() {
         long playedFrames = 0;
-        byte[] tempBuffer = new byte[FRAME_SIZE];
+        byte[] tempBuffer = new byte[frameSize];
 
         while(playedFrames < size) {
-            audioBuffer.get(tempBuffer, 0, FRAME_SIZE);
-            dataLine.write(tempBuffer, 0, FRAME_SIZE);
+            audioBuffer.get(tempBuffer, 0, frameSize);
+            dataLine.write(tempBuffer, 0, frameSize);
             ++playedFrames;
         }
     }
