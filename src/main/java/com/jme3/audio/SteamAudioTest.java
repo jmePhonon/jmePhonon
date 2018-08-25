6d270a11856f6e9109148fa6d05504bd3cd4c005
aegroto <aegroto@protonmail.com>
2018-08-25 16:38:31 +0200

    added jME dependency

---
+++ src/main/java/com/jme3/audio/SteamAudioTest.java
@@ -0,0 +1,15 @@
+package com.jme3.audio;
+
+import com.jme3.app.SimpleApplication;
+
+public class SteamAudioTest extends SimpleApplication {
+    public static void main(String[] args) {
+        SteamAudioTest app = new SteamAudioTest();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+
+    }
+}
