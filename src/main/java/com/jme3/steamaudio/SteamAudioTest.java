f2d364be7ac98ab12a29fa6836f80025a140f325
aegroto <aegroto@protonmail.com>
2018-08-25 18:18:47 +0200

    simple player with a test random generated sound added

--- src/main/java/com/jme3/steamaudio/SteamAudioTest.java
+++ src/main/java/com/jme3/steamaudio/SteamAudioTest.java
@@ -1,6 +1,10 @@
 package com.jme3.steamaudio;
 
+import java.nio.ByteBuffer;
+
 import com.jme3.app.SimpleApplication;
+import com.jme3.util.BufferUtils;
+import com.jme3.steamaudio.player.SteamAudioPlayer;
 
 public class SteamAudioTest extends SimpleApplication {
     public static void main(String[] args) {
@@ -10,5 +14,13 @@ public class SteamAudioTest extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        int bufferSize = 256;
+        byte[] testData = new byte[bufferSize];
+        for(int i = 0; i < bufferSize; ++i) {
+            testData[i] = (byte) (i % 128);
+        }
+        ByteBuffer testBuffer = BufferUtils.createByteBuffer(testData);
+        SteamAudioPlayer player = new SteamAudioPlayer(testBuffer, bufferSize / SteamAudioPlayer.FRAME_SIZE, 10, 8);
+        player.play();
     }
 }
