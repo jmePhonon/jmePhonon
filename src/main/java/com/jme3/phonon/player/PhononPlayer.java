c2f848fe4a6d6a714faac9c34193ca9acf89fbb8
aegroto <aegroto@protonmail.com>
2018-08-31 00:25:51 +0200

    phonon player and buffer documentation

--- src/main/java/com/jme3/phonon/player/PhononPlayer.java
+++ src/main/java/com/jme3/phonon/player/PhononPlayer.java
@@ -1,26 +1,16 @@
 package com.jme3.phonon.player;
 
-import java.nio.ByteBuffer;
-import java.nio.channels.Channel;
-import java.util.Arrays;
-
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
-import com.jme3.phonon.BitUtils;
-import com.jme3.phonon.F32leAudioData;
 import com.jme3.phonon.PhononOutputChannel;
-import com.jme3.phonon.PhononOutputChannel.ChannelStatus;
-
-import org.lwjgl.BufferUtils;
 
 public class PhononPlayer {
-    public final static int FRAMES_IN_BUFFER = 2;
+    public final static int FRAMES_IN_BUFFER = 16;
 
     public final PhononOutputChannel phononChannel;
-    // public final long totalFrames;
 
     private final SourceDataLine dataLine;
     private final AudioFormat audioFormat;
@@ -28,15 +18,18 @@ public class PhononPlayer {
     public final int channels;
 
     private boolean inPlayback = false;
-    private int samplesBytes, remainingBytes, bufferFrameSize, bufferIndex;
-    private byte[] floatFrame, intBuffer;
+    
+    private PhononPlayerBuffer buffer;
 
     /**
-     * @param audioData Float encoded audio data
+     * Player object for jME's Phonon interface.
+     * 
+     * @param chan Phonon channel to stream on
+     * @param channels Number of audio channels
      * @param sampleSize Either 8 16 or 24
-     * @param bufferedFrames How many frames are buffered before sending the data to the audio device.
+     * 
+     * @author aegroto, riccardobl
      */
-
 
     public PhononPlayer(PhononOutputChannel chan,int channels, int sampleSize)
             throws LineUnavailableException {
@@ -49,27 +42,31 @@ public class PhononPlayer {
         dataLine = AudioSystem.getSourceDataLine(audioFormat);
         dataLine.open(audioFormat, chan.getBufferSize() * audioFormat.getFrameSize());
 
+        buffer = new PhononPlayerBuffer(FRAMES_IN_BUFFER, audioFormat.getSampleSizeInBits(), phononChannel);
     }
+
+    /**
+     * Starts playback.
+     * 
+     * @author aegroto
+     */
 
     public void startPlayback() {
-        samplesBytes = (audioFormat.getSampleSizeInBits() / 8);
-        bufferFrameSize = phononChannel.getFrameSize() * samplesBytes;
-        floatFrame = new byte[phononChannel.getFrameSize() * 4];
-        intBuffer = new byte[bufferFrameSize * FRAMES_IN_BUFFER];
-
-        inPlayback = true;
-
-        remainingBytes = 0;   
+        inPlayback = true; 
         
-        for(int i = 0; i < FRAMES_IN_BUFFER; ++i) {
-            loadNextFrame();
-        }
+        buffer.fillBuffer();
     }
 
+    /**
+     * Proceed with next playback step.
+     * 
+     * @author aegroto, riccardobl
+     */
+
     public void continuePlayback() {
-        if(remainingBytes > 0) {
+        if(buffer.getRemainingFrameBytes() > 0) {
             int available = dataLine.available();
-            int writable = remainingBytes > available ? available : remainingBytes;
+            int writable = buffer.getRemainingFrameBytes() > available ? available : buffer.getRemainingFrameBytes();
 
             if (writable >  available) {
                 System.err.println("FIX ME: " + writable 
@@ -77,8 +74,7 @@ public class PhononPlayer {
                         + " left. This will cause the thread to stop and wait until more bytes are available");
             }
 
-            dataLine.write(intBuffer, (bufferIndex * bufferFrameSize) + bufferFrameSize - remainingBytes, writable);
-            remainingBytes -= writable;
+            buffer.writeToLine(dataLine, writable); 
 
             // Start the dataLine if it is not playing yet. 
             // We do this here to be sure there is some data already available to be played
@@ -86,59 +82,17 @@ public class PhononPlayer {
                 dataLine.start();
         }
 
-        if(remainingBytes == 0) {        
-            loadNextFrame();
+        if(buffer.getRemainingFrameBytes() == 0) {        
+            inPlayback = buffer.loadNextFrame();
         }    
     }
-
-    private void loadNextFrame() {
-        ChannelStatus stat = phononChannel.readNextFrameForPlayer(floatFrame);
-        switch (stat) {
-            case NODATA:
-                System.err.println("No data to read. Phonon is lagging behind");
-                break;
-            case OVER:
-                inPlayback = false;
-                System.out.println("Audio data is over");
-                break;
-            case READY:
-                // System.out.println("Playing");
-                // Convert to proper encoding
-                convertFloats(floatFrame, intBuffer, bufferIndex * bufferFrameSize);
-                bufferIndex = (bufferIndex + 1) % FRAMES_IN_BUFFER;
-                remainingBytes = bufferFrameSize;
-        }
-    }
-
+    
+    /**
+     * @return true if player is in playback, false otherwise.
+     * 
+     * @author aegroto
+     */
     public boolean isInPlayback() {
         return inPlayback;
-    }
-
-    private void convertFloat(byte[] inputBuffer, byte[] outputBuffer) {
-        // Convert to the proper output format for this source line
-        if (audioFormat.getSampleSizeInBits() == 8) {
-            BitUtils.cnvF32leToI8le(inputBuffer, outputBuffer);
-        } else if (audioFormat.getSampleSizeInBits() == 16) {
-            BitUtils.cnvF32leToI16le(inputBuffer, outputBuffer);
-        } else {
-            BitUtils.cnvF32leToI24le(inputBuffer, outputBuffer);
-        }
-    }
-    
-    private void convertFloats(byte[] inb, byte[] outb, int offset) {
-        byte[] partInputBuffer = new byte[4];
-        byte[] partOutputBuffer = new byte[audioFormat.getSampleSizeInBits() / 8];
-
-        for (int i = 0; i < inb.length; i+=4) {
-            partInputBuffer[0] = inb[i];
-            partInputBuffer[1] = inb[i+1];
-            partInputBuffer[2] = inb[i + 2];
-            partInputBuffer[3] = inb[i+3];
-           
-            convertFloat(partInputBuffer, partOutputBuffer);
-
-            for(int j = 0; j < partOutputBuffer.length; ++j)
-                outb[offset + (i/4) * partOutputBuffer.length + j] = partOutputBuffer[j];
-        }
     }
 }
--- src/main/java/com/jme3/phonon/player/PhononPlayerBuffer.java
+++ src/main/java/com/jme3/phonon/player/PhononPlayerBuffer.java
@@ -1,5 +1,145 @@
 package com.jme3.phonon.player;
 
-class PhononPlayerBuffer {
+import javax.sound.sampled.SourceDataLine;
 
+import com.jme3.phonon.BitUtils;
+import com.jme3.phonon.PhononOutputChannel;
+import com.jme3.phonon.PhononOutputChannel.ChannelStatus;
+
+class PhononPlayerBuffer {
+    public final int sizeInFrames, sampleSize, bufferFrameSize;
+    public final byte[] floatFrame, intBuffer;
+
+    public final PhononOutputChannel phononChannel;
+
+    private int remainingBytes, bufferIndex;
+
+    /**
+     * An auxiliary class containing a buffer for PhononPlayer.
+     * 
+     * @param sizeInFrames Size of buffer in frames
+     * @param sampleSize Samples size
+     * @param channel Phonon channel to stream on
+     * 
+     * @author aegroto
+     */
+
+    PhononPlayerBuffer(int sizeInFrames, int sampleSize, PhononOutputChannel channel) {
+        this.sizeInFrames = sizeInFrames;
+        this.phononChannel = channel;
+        this.sampleSize = sampleSize;
+
+        this.bufferFrameSize = channel.getFrameSize() * sampleSize / 8;
+        this.floatFrame = new byte[channel.getFrameSize() * 4];
+        this.intBuffer = new byte[bufferFrameSize * sizeInFrames];
+    }
+
+    /**
+     * Fills the buffer when it is empty after creation. SHOULD NOT BE CALLED.
+     * 
+     * @author aegroto
+     */
+
+    public void fillBuffer() {
+        for(int i = 0; i < sizeInFrames; ++i) {
+            loadNextFrame();
+        }
+    }
+    /**
+     * @return Remaining bytes in the current loaded frame
+     * 
+     * @author aegroto
+     */
+
+    public int getRemainingFrameBytes() {
+        return remainingBytes;
+    }
+
+    /**
+     * Loads next frame in the buffer.
+     * 
+     * @return true if playback should be able to continue, false otherwise.
+     * 
+     * @author aegroto
+     */
+    
+    public boolean loadNextFrame() {
+        ChannelStatus stat = phononChannel.readNextFrameForPlayer(floatFrame);
+        switch (stat) {
+            case NODATA:
+                System.err.println("No data to read. Phonon is lagging behind");
+                break;
+            case OVER:
+                System.out.println("Audio data is over");
+                return false; 
+            case READY:
+                // Convert to proper encoding
+                convertFloats(floatFrame, intBuffer, bufferIndex * bufferFrameSize);
+                bufferIndex = (bufferIndex + 1) % sizeInFrames;
+                remainingBytes = bufferFrameSize;
+        }
+
+        return true;
+    }
+
+    /**
+     * Called by PhononPlayer to write frame data on a SourceDataLine.
+     * 
+     * @param dataLine SourceDataLine to be written
+     * @param bytesToWrites Number of bytes to write on the line
+     * 
+     * @author aegroto
+     */
+
+    public void writeToLine(SourceDataLine dataLine, int bytesToWrite) {
+        dataLine.write(intBuffer, (bufferIndex * bufferFrameSize) + bufferFrameSize - remainingBytes, bytesToWrite);
+        remainingBytes -= bytesToWrite;
+    }
+   
+    /**
+     * Auxiliary method to decode a float to an int.
+     * 
+     * @param inputBuffer Input buffer
+     * @param outputBuffer Output buffer
+     * 
+     * @author aegroto
+     */
+
+    private void convertFloat(byte[] inputBuffer, byte[] outputBuffer) {
+        // Convert to the proper output format for this source line
+        if (sampleSize == 8) {
+            BitUtils.cnvF32leToI8le(inputBuffer, outputBuffer);
+        } else if (sampleSize == 16) {
+            BitUtils.cnvF32leToI16le(inputBuffer, outputBuffer);
+        } else {
+            BitUtils.cnvF32leToI24le(inputBuffer, outputBuffer);
+        }
+    }
+
+    /**
+     * Auxiliary method to decode multiple floats to an ints.
+     * 
+     * @param inputBuffer Input buffer
+     * @param outputBuffer Output buffer
+     * @param offset Output buffer's write offset
+     * 
+     * @author aegroto
+     */
+
+    private void convertFloats(byte[] inb, byte[] outb, int offset) {
+        byte[] partInputBuffer = new byte[4];
+        byte[] partOutputBuffer = new byte[sampleSize / 8];
+
+        for (int i = 0; i < inb.length; i+=4) {
+            partInputBuffer[0] = inb[i];
+            partInputBuffer[1] = inb[i+1];
+            partInputBuffer[2] = inb[i + 2];
+            partInputBuffer[3] = inb[i+3];
+           
+            convertFloat(partInputBuffer, partOutputBuffer);
+
+            for(int j = 0; j < partOutputBuffer.length; ++j)
+                outb[offset + (i/4) * partOutputBuffer.length + j] = partOutputBuffer[j];
+        }
+    }
 }
